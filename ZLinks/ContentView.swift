5b0e18fbd7da4c8b4b4071adc5ea59e911be3a9b
CO <bptpw_service@163.com>
2026-09-10 18:26:54 +0800

    Initial Commit

---
+++ ZLinks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZLinks
+//
+//  Created by co on 2026/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZLinks/ZLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZLinksApp.swift
+//  ZLinks
+//
+//  Created by co on 2026/9/10.
+//
+
+import SwiftUI
+
+@main
+struct ZLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
